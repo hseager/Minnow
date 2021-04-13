26e01ff818be6dc681dae6bba9cbcd7b32c0a736
hseager <harvey.seager@googlemail.com>
2021-04-13 17:10:32 +0100

    Add project files.

---
+++ YouTubeManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoLibrary;
+
+namespace Minnow
+{
+    static class YouTubeManager
+    { 
+        public static void GetYouTubeVideo(string url)
+        {
+            YouTube youtube = YouTube.Default;
+            var videos = Client.For(YouTube.Default).GetAllVideosAsync(url).GetAwaiter().GetResult();
+
+            var test = videoInfos;
+        }
+
+
+    }
+}
